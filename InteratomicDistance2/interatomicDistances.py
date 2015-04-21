464c0fbb20c57bcab94aec483c554b52db51b6fb
Layton <kuddai92@yandax.ru>
2015-04-21 17:19:49 +0300

    added acell parsing feature to python version

--- InteratomicDistance2/interatomicDistances.py
+++ InteratomicDistance2/interatomicDistances.py
@@ -1,57 +1,72 @@
-import sys, re
+import sys
+import numpy as np
 
-def get_steps(file_name):
-    def get_raw_steps():
-        """we must extract data patterns like this one:
+def get_data(file_name):
+    import re
+    def get_text():
+        with open(file_name) as file:
+            return file.read()
+
+    def get_raw_acell(text):
+        """we must extract acell pattern like this one:
+            acell      7.6962876214E+00  7.6962876214E+00  7.6962876214E+00 Bohr
+              amu      1.06000000E+02  2.00000000E+00              
+        """
+        acell_pattern = re.compile(r"""
+            acell\s+#header - indicate acell parameter
+            (?P<acell>#Catch acell values in Bohr
+                (?:#Match excatly 3 numbers. We don't catch this group
+                    (?:-?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)\s+#number in scientific notation. We don't catch this group
+                ){3}
+            )Bohr
+            """, re.VERBOSE)
+        match = re.search(acell_pattern, text) 
+        return match.group("acell")
+
+    def get_raw_steps(text):
+        """we must extract steps like this one:
             Cartesian coordinates (xcart) [bohr]
             1.75255604597822E-03  2.44698778617683E-03  3.18934151739078E-05
             3.84712955498547E+00 -1.92096780059473E-03  3.84685455237236E+00
             2.47640757288980E-03  3.84891394970560E+00  3.84925589870608E+00     
-        """
-        with open(file_name) as file:
-            text = file.read()
-            pattern = re.compile(r"""
-                Cartesian\scoordinates\s\(xcart\)\s\[bohr\]\s+#header - indicate that there are atom coordinates in bohr after it
-                ( #catch pattern (number in scientific notation with possible spaces after it) as many times as you can  
-                    (?: #Match spaces and new lines after number. We don't wanna catch this group as well
-                        (?:-?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)#number in scientific notation. We don't catch this group
-                    \s+)
-                +)
-                """, re.VERBOSE)
-            raw_steps = re.findall(pattern, text)     
-            return raw_steps
-
-    def group(lst, n):        
-        """group([0,3,4,10,2,3], 2) => iterator
-
-        Group an iterable into an n-tuples iterable. Incomplete tuples
-        are discarded e.g.
-
-        >>> list(group(range(10), 3))
-        [(0, 1, 2), (3, 4, 5), (6, 7, 8)]
-        """
-        from itertools import islice
-        return zip(*[islice(lst, i, None, n) for i in range(n)])   
-
-    raw_steps = get_raw_steps()
-    steps = [group([float(pos) for pos in rs.split()], 3) for rs in raw_steps]
-    return steps
-
-def main(file_name):
-    print file_name
-    with open(file_name) as file:
-        text = file.read()
-        pattern = re.compile(r"""
+        """       
+        step_pattern = re.compile(r"""
             Cartesian\scoordinates\s\(xcart\)\s\[bohr\]\s+#header - indicate that there are atom coordinates in bohr after it
             ( #catch pattern (number in scientific notation with possible spaces after it) as many times as you can  
-                (?: #Match spaces and new lines after number. We don't wanna catch this group as well
+                (?: #Match spaces and new lines after number. We don't catch this group
                     (?:-?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)#number in scientific notation. We don't catch this group
                 \s+)
             +)
-            """, re.VERBOSE)
-        raw_steps = re.findall(pattern, text)#raw array of MD steps
-        steps = [group([float(pos) for pos in rs.split()], 3) for rs in raw_steps]
-        print steps[1]
+            """, re.VERBOSE)   
+        return re.findall(step_pattern, text)   
+
+    def get_numbers(raw_numbers):
+        numbers = [float(raw_num) for raw_num in raw_numbers.split()]
+        return np.array(numbers)
+
+    def get_steps(raw_steps):
+        def get_step(raw_step):
+            coords = get_numbers(raw_step)
+            step = coords.reshape(-1, 3)
+            return step
+        return [get_step(rs) for rs in raw_steps]
+
+    text = get_text()
+    raw_acell, raw_steps = get_raw_acell(text), get_raw_steps(text)
+    acell, steps = get_numbers(raw_acell), get_steps(raw_steps)
+    return (acell, steps)
+
+def get_acell(file_name):
+    pass
+
+def calc_distances():
+    pass
+
+def main(file_name):
+    acell, steps = get_data(file_name)
+    print acell
+    print steps[0]
+    #print steps[0]
 
 if __name__ == "__main__":
     main(sys.argv[1])

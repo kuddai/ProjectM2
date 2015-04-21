8cc117f27f2b6609cc0cde991226aceed624d75a
Layton <kuddai92@yandax.ru>
2015-04-21 12:22:26 +0300

    initial commit

---
+++ InteratomicDistance/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿
+$(function () {
+    $("#file-input").on('fileloaded', function (event, file, previewId, index, reader) {
+        $("#preview-header").text("Предпросмотр: " + file.name);
+        reader.onload = function (e) {
+            var contents = e.target.result;
+            $("#preview").text(contents);
+        };
+        reader.readAsText(file);
+    });
+    function parseData(rawData) {
+
+    }
+});
